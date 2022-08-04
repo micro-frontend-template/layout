490ccbfde67fa7b2e2075cf3dff762ee5bd13546
hotien2107 <hotien2107@gmail.com>
2022-08-04 15:17:39 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+// remote/webpack.config.js
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
+const { dependencies } = require("./package.json");
+
+module.exports = {
+  entry: "./src/index",
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 3002,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "Layout",
+      filename: "moduleEntry.js",
+      exposes: {
+        "./AdminNavbar": "./src/components/Navbars/AdminNavbar",
+        "./HeaderStats": "./src/components/Headers/HeaderStats",
+        "./AdminDashboard": "./src/views/Dashboard",
+        "./AdminSettings": "./src/views/Settings",
+        "./AdminTables": "./src/views/Tables",
+        "./FooterAdmin": "./src/components/Footers/FooterAdmin",
+      },
+      shared: {
+        ...dependencies,
+        react: {
+          singleton: true,
+          requiredVersion: dependencies["react"],
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: dependencies["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  target: "web",
+};
